e93e428181b08467b993734dedcc7a644ef6baa9
Samy GHRIBI <ghribi.samy@gmail.com>
2026-07-09 18:18:50 +0200

    Protect Willie data during Aquasys ingestion

--- src/connectors/aquasys.ts
+++ src/connectors/aquasys.ts
@@ -525,7 +525,7 @@ export class AquasysConnector extends BaseConnector<
   private static readonly connectorEnabledDate = new Date('2026-01-01')
   private static readonly metric = {
     unit: MetricUnit.M3,
-    conflictPolicy: ConflictPolicy.SKIP_CONFLICTING_VALUES,
+    conflictPolicy: ConflictPolicy.REPLACE_EXISTING_EXCEPT_WILLIE,
   } as const
 
   constructor() {
